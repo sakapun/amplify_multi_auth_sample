7e30234c9a2c5d57ba893477140b6668d1f4cc97
sakamoto.m <sakamoto.m@water-cell.jp>
2020-04-27 01:33:12 +0900

    整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {ABC} from "./component/ABC";
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import {PostAdd} from "./component/PostAdd";
 import {Provider} from "./application/provider";
 import {IndexPage} from "./presentation/container/IndexPage";
 
